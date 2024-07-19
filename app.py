19922db328f80e09c2531f74296a0c9504a030bd
Debarka <debarka.2k2@gmail.com>
2024-07-19 18:26:26 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,50 @@
+import os
+import streamlit as st
+from dotenv import load_dotenv
+from langchain.chains import RetrievalQA
+from langchain_community.document_loaders import WebBaseLoader
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.llms import Ollama
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_community.vectorstores import Chroma
+
+def main():
+    st.title('ChatLink- Chat With Website')
+    st.subheader('Input your website URL, ask questions, and receive answers directly from the website.')
+
+    url = st.text_input("Insert The website URL")
+
+    prompt = st.text_input("Ask a question (query/prompt)")
+    if st.button("Submit Query", type="primary"):
+        ABS_PATH: str = os.path.dirname(os.path.abspath(__file__))
+        DB_DIR: str = os.path.join(ABS_PATH, "db")
+
+        loader = WebBaseLoader(url)
+        data = loader.load()
+
+        text_splitter = CharacterTextSplitter(separator='\n', 
+                                        chunk_size=1000, 
+                                        chunk_overlap=40)
+
+        docs = text_splitter.split_documents(data)
+
+        ollama_embeddings = OllamaEmbeddings(model="mistral")
+
+        vectordb = Chroma.from_documents(documents=docs, 
+                                        embedding=ollama_embeddings,
+                                        persist_directory=DB_DIR)
+
+        vectordb.persist()
+
+        retriever = vectordb.as_retriever(search_kwargs={"k": 3})
+
+        llm = Ollama(model="mistral")
+
+        qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever)
+
+        response = qa(prompt)
+        st.write(response)
+        
+
+if __name__ == '__main__':
+    main()
